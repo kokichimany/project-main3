2aa8fff26241b3dd7fd570724780ad1fb18da648
kokichimany <aslanbekovaajelina@gmail.com>
2025-06-13 18:10:56 +0600

    Update forms.py

--- store/forms.py
+++ store/forms.py
@@ -1,7 +1,17 @@
 from django import forms
-from .models import Comment
+from .models import Comment, Post, PostComment
 
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
+        fields = ['text']
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'content', 'image']
+
+class PostCommentForm(forms.ModelForm):  
+    class Meta:
+        model = PostComment
         fields = ['text']
