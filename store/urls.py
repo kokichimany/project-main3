fcb975a77b5a89356b92abcb91a720fe6143cdf5
kokichimany <aslanbekovaajelina@gmail.com>
2025-06-13 18:10:17 +0600

    Update urls.py

--- store/urls.py
+++ store/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.product_list, name='product_list'),
     path('product/<int:pk>/', views.product_detail, name='product_detail'),
-    path('register/', views.register, name='register'),
+    path('posts/', views.post_list, name='post_list'),
+    path('posts/<int:pk>/', views.post_detail, name='post_detail'),
+    path('posts/new/', views.post_create, name='post_create'),
+    path('register/', views.register, name='register'),  # ← Регистрация
 ]
